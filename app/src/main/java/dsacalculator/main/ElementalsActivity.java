dda63288c67d4c59753ef406358eb0b5e55f67a9
Mathias von Behren <mathias.behren@brainsphere.de>
2019-04-30 14:49:18 +0200

    First commit with initial work

---
+++ app/src/main/java/dsacalculator/main/ElementalsActivity.java
@@ -0,0 +1,13 @@
+package dsacalculator.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ElementalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elementals);
+    }
+}
---
+++ app/src/main/java/dsacalculator/main/PotionsActivity.java
@@ -0,0 +1,16 @@
+package dsacalculator.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PotionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_potions);
+
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+    }
+}
